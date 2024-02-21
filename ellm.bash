ad24f6cebbb3288340ab39f0e5fe41d9aa2a3352
indro <indro@orin.lan>
2024-02-21 11:56:46 -0500

    Default command script

---
+++ ellm.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+./set-emeet-sink.sh
+mic=$(grep -oP '(?<=Device #).*?(?=: Chat 150 Mono)' <<< $(~/.local/bin/cheetah_demo_mic --show_audio_devices))
+echo "Microphone Device Index: ${mic}."
+python3 ~/embodied_llm/embodied_llm/agent/ellm.py --microphone=${mic} --models-folder=/home/indro/ellm --camera=5 --send-commands --remote
